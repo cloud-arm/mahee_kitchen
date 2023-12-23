4bdc4919c9f3d45b57c25ff8b50f9cb84887d08a
eranda-palliyaguru <erandasampath2000@gmail.com>
2023-12-23 17:32:29 +0530

    update

--- product_edit_save.php
+++ product_edit_save.php
@@ -1,31 +1,22 @@
 <?php
 session_start();
 include('connect.php');
-$a = $_POST['sell'];
-$b = $_POST['cost'];
+$sell = $_POST['sell'];
+$cost = $_POST['cost'];
 $name = $_POST['name'];
+$cat_id=$_POST['cat_id'];
 $id =$_POST['id'];
 
 
-
-$sql = "UPDATE products 
-        SET sell_price=?
-		WHERE id=?";
-$q = $db->prepare($sql);
-$q->execute(array($a,$id));
-
-$sql = "UPDATE products 
-        SET cost_price=?
-		WHERE id=?";
-$q = $db->prepare($sql);
-$q->execute(array($b,$id));
+$result = $db->prepare('SELECT * FROM category WHERE  id=:id ');
+$result->bindParam(':id', $cat_id);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){ $cat=$row['name']; }
 
 
-$sql = "UPDATE products 
-        SET product_name=?
-		WHERE id=?";
-$q = $db->prepare($sql);
-$q->execute(array($name,$id));
+$sql = 'UPDATE  products SET product_name=?, sell_price =? , cost_price=?, cat=?, cat_id=?  WHERE  id=? ';
+$ql = $db->prepare($sql);
+$ql->execute(array($name,$sell,$cost,$cat,$cat_id,$id));
 
 
 header("location: product_view.php");
